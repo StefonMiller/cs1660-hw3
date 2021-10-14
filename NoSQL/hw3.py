67a5d52cdfa144dc56413295df7e323620a645bd
Stefon Miller <35376880+StefonMiller@users.noreply.github.com>
2021-10-14 12:22:53 -0400

    Removed errors from output

--- NoSQL/hw3.py
+++ NoSQL/hw3.py
@@ -2,20 +2,20 @@ import boto3
 import csv
 
 s3 = boto3.resource('s3',
-    aws_access_key_id='AKIA2P3ULVTGU63VEC33',
-    aws_secret_access_key='FAkeKkd9n4xh2baTJ3YNlbc5MfIipDn1heS3vNas'
+    aws_access_key_id='aws_access_key',
+    aws_secret_access_key='aws_secret_key'
 )
 
 try:
     s3.create_bucket(Bucket='cs1660-hw3', CreateBucketConfiguration={
     'LocationConstraint': 'us-east-2'})
 except Exception as e:  
-    print (e)
+    pass
 
 bucket = s3.Bucket("cs1660-hw3")
 bucket.Acl().put(ACL='public-read')
 
-body = open('exp1.csv', 'rb')
+body = open('NoSQL/exp1.csv', 'rb')
 
 o = s3.Object('cs1660-hw3', 'test').put(Body=body)
 
@@ -23,8 +23,8 @@ s3.Object('cs1660-hw3', 'test').Acl().put(ACL='public-read')
 
 dyndb = boto3.resource('dynamodb',
     region_name='us-east-2',
-    aws_access_key_id='AKIA2P3ULVTGU63VEC33',
-    aws_secret_access_key='FAkeKkd9n4xh2baTJ3YNlbc5MfIipDn1heS3vNas')
+    aws_access_key_id='aws_access_key',
+    aws_secret_access_key='aws_secret_key')
 try:
     table = dyndb.create_table(
     TableName='DataTable',
@@ -49,20 +49,19 @@ try:
         'WriteCapacityUnits': 5
     })
 except Exception as e:
-    print (e)
+    pass
     #if there is an exception, the table may already exist. if so...
     table = dyndb.Table("DataTable")
 
 #wait for the table to be created
 table.meta.client.get_waiter('table_exists').wait(TableName='DataTable')
-print(table.item_count)
 
-with open('experiments.csv', 'r') as csvfile:
+with open('NoSQL/experiments.csv', 'r') as csvfile:
     csvf = csv.reader(csvfile, delimiter=',', quotechar='|')
     next(csvf)
     
     for item in csvf:
-        body = open(item[4], 'rb')
+        body = open('NoSQL/' + item[4], 'rb')
         s3.Object('cs1660-hw3', item[4]).put(Body=body )
         md = s3.Object('cs1660-hw3', item[4]).Acl().put(ACL='public-read')
 
@@ -72,7 +71,7 @@ with open('experiments.csv', 'r') as csvfile:
         try:
             table.put_item(Item=metadata_item)
         except:
-            print("item may already be there or another failure")
+            pass
 
 response = table.get_item(
     Key={
